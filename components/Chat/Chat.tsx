9a28c19eb3c256d356946455db932df6060d4583
Guillermo <103156657+guillermazo@users.noreply.github.com>
2023-07-21 01:57:44 -0600

    Update Guidev Studios

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -378,7 +378,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Chatbot UI'
+                      'CHAT GPT'
                     )}
                   </div>
 
