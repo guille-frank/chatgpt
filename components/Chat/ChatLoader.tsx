d9646f1bd996fd892d197044655b7cf54cc8adbc
Guillermo <mefranco@live.com>
2023-07-29 16:05:24 -0600

    Actualización Graciosa

--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -1,9 +1,48 @@
-import { IconRobot } from '@tabler/icons-react';
+import { IconBrandAmongUs } from '@tabler/icons-react';
 import { FC } from 'react';
 
 interface Props { }
 
 export const ChatLoader: FC<Props> = () => {
+  const randomTexts = [
+    'Pensando',
+    'Psicoanalizándote',
+    'Defecando',
+    'Planificando',
+    'Innovando',
+    'Investigando',
+    'Analizando',
+    'Resolviendo',
+    'Calculando',
+    'Creando',
+    'Optimizando',
+    'Escribiendo',
+    'Improvisando',
+    'Deliberando',
+    'Proporcionando',
+    'Comunicando',
+    'Aprendiendo',
+    'Colaborando',
+    'Transformando',
+    'Descubriendo',
+    'Equilibrando',
+    'Reflexionando',
+    'Interpretando',
+    'Decodificando',
+    'Observando',
+    'Sintetizando',
+    'Elaborando',
+    'Experimentando',
+    'Sorprendiendo',
+    'Persiguiendo',
+    'Cuestionando',
+    'Sonriendo',
+    'Sorprendiéndote',
+    'Investigando como te podría engañar'
+  ];
+  const randomIndex = Math.floor(Math.random() * randomTexts.length);
+  const randomText = randomTexts[randomIndex];
+
   return (
     <div
       className="group border-b border-black/10 bg-gray-50 text-gray-800 dark:border-gray-900/50 dark:bg-[#444654] dark:text-gray-100"
@@ -11,9 +50,9 @@ export const ChatLoader: FC<Props> = () => {
     >
       <div className="m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[40px] items-end">
-          <IconRobot size={30} />
+          <IconBrandAmongUs size={30} />
         </div>
-        <span className="animate-pulse cursor-default mt-1">▍</span>
+        <span className="cursor-default animate-pulse  mt-1">{randomText}...▍</span>
       </div>
     </div>
   );
