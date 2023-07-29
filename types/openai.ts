1dd2e70c981173aeb768ed61fdf7007298394c2d
Guillermo <mefranco@live.com>
2023-07-28 18:10:05 -0600

    Update OpenAi

--- types/openai.ts
+++ types/openai.ts
@@ -12,7 +12,7 @@ export enum OpenAIModelID {
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
-  GPT_3_5_16K = 'gpt-35-turbo-16k',
+  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
