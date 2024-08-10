048d8e3937c8f029cccf57b7e13d7651d7f8311a
Guillermo <103156657+guille-frank@users.noreply.github.com>
2024-08-10 12:19:19 -0600

    Update sessions.ts

--- utils/app/sessions.ts
+++ utils/app/sessions.ts
@@ -2,22 +2,24 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-
 // Función para verificar la sesión del usuario
 export const verifySession = async (first: boolean) => {
   // Obtener el valor de la cookie "wp-gstools_login"
   const cookieValue = getCookieValue('wp-gstools_login');
 
+  // Imprimir el valor de la cookie en la consola
+  console.log('Valor de la cookie wp-gstools_login:', cookieValue);
+
   if (!cookieValue) {
     if (first) {
       toast.info('¡Inicia sesión primero!', {
-        position: 'top-center', // Posición de la notificación en la pantalla
-        autoClose: 3000, // Tiempo en milisegundos que la notificación se mostrará antes de cerrarse automáticamente
-        hideProgressBar: false, // Mostrar barra de progreso
-        closeOnClick: true, // Cerrar la notificación al hacer clic en ella
-        pauseOnHover: true, // Pausar el tiempo de cierre al pasar el cursor sobre la notificación
-        draggable: true, // Permitir arrastrar la notificación
-        progress: undefined, // Componente personalizado para la barra de progreso
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
       });
     }
     // Si la cookie no existe, retornar false
@@ -25,28 +27,27 @@ export const verifySession = async (first: boolean) => {
   } else {
     try {
       const headers = {
-        // Agrega encabezados personalizados aquí
         'User-Agent': navigator.userAgent,
         'Accept': "application/json",
         'DNT': "1",
-        // Otros encabezados si es necesario
       };
       // Hacer una solicitud al servidor para verificar la cookie
       const response = await axios.get(
-        `https://backoffice.guidevstudios.com/wp-gstools/wp-json/gs/v1/check_cookie/${cookieValue}`);
+        `https://backoffice.guidevstudios.com/wp-gstools/wp-json/gs/v1/check_cookie/${cookieValue}`
+      );
 
       // Comprobar si el servidor retorna un array con "exists": true
       if (response.data.exists) {
         return true;
       } else {
         toast.info('Parece que tu sesión se caducó.', {
-          position: 'top-center', // Posición de la notificación en la pantalla
-          autoClose: 5000, // Tiempo en milisegundos que la notificación se mostrará antes de cerrarse automáticamente
-          hideProgressBar: false, // Mostrar barra de progreso
-          closeOnClick: true, // Cerrar la notificación al hacer clic en ella
-          pauseOnHover: true, // Pausar el tiempo de cierre al pasar el cursor sobre la notificación
-          draggable: true, // Permitir arrastrar la notificación
-          progress: undefined, // Componente personalizado para la barra de progreso
+          position: 'top-center',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
         });
         return false;
       }
@@ -54,13 +55,13 @@ export const verifySession = async (first: boolean) => {
       // Si hay algún error en la solicitud al servidor, retornar false
       console.error('Error al verificar la cookie:', error);
       toast.error('Parece que no hay conexión con el servidor...\nReintentando...', {
-        position: 'top-center', // Posición de la notificación en la pantalla
-        autoClose: 8000, // Tiempo en milisegundos que la notificación se mostrará antes de cerrarse automáticamente
-        hideProgressBar: false, // Mostrar barra de progreso
-        closeOnClick: true, // Cerrar la notificación al hacer clic en ella
-        pauseOnHover: true, // Pausar el tiempo de cierre al pasar el cursor sobre la notificación
-        draggable: true, // Permitir arrastrar la notificación
-        progress: undefined, // Componente personalizado para la barra de progreso
+        position: 'top-center',
+        autoClose: 8000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
       });
       return null;
     }
