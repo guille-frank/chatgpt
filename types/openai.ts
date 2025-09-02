6ba457e90075bb184a80a2815bbb65545cacaa46
Guillermo <103156657+guille-frank@users.noreply.github.com>
2025-09-02 17:54:52 -0600

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -10,43 +10,17 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
   GPT_4_turbo = 'gpt-4-turbo-preview',
-  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_4O = 'gpt-4o',
+  GPT_5_THINKING = 'gpt-5-thinking',
+  GPT_5_INSTANT = 'gpt-5-instant',
+  GPT_5 = 'gpt-5',
 }
-//uwu
 
-// in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+// en caso de que `DEFAULT_MODEL` no esté definido o sea inválido
+export const fallbackModelID = OpenAIModelID.GPT_4_turbo;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 2990,
-    tokenLimit: 4000,
-    tokenLimitMargin: 1010,
-    maxMessages: 10,
-  },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 2990,
-    tokenLimit: 4000,
-    tokenLimitMargin: 1010,
-    maxMessages: 10,
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
-    maxLength: 6800,
-    tokenLimit: 8100,
-    tokenLimitMargin: 1300,
-    maxMessages: 15,
-  },
   [OpenAIModelID.GPT_4_turbo]: {
     id: OpenAIModelID.GPT_4_turbo,
     name: 'GPT-4-Turbo',
@@ -54,14 +28,6 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     tokenLimit: 128000,
     tokenLimitMargin: 1600,
     maxMessages: 20,
-  },
-  [OpenAIModelID.GPT_3_5_16K]: {
-    id: OpenAIModelID.GPT_3_5_16K,
-    name: 'GPT-3.5-16K',
-    maxLength: 15100,
-    tokenLimit: 16300,
-    tokenLimitMargin: 1200,
-    maxMessages: 12,
   },
   [OpenAIModelID.GPT_4O]: {
     id: OpenAIModelID.GPT_4O,
@@ -71,4 +37,29 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     tokenLimitMargin: 3000,
     maxMessages: 20,
   },
+  [OpenAIModelID.GPT_5_THINKING]: {
+    id: OpenAIModelID.GPT_5_THINKING,
+    name: 'GPT-5-Thinking',
+    maxLength: 15100,
+    tokenLimit: 128000,
+    tokenLimitMargin: 3000,
+    maxMessages: 20,
+  },
+  [OpenAIModelID.GPT_5_INSTANT]: {
+    id: OpenAIModelID.GPT_5_INSTANT,
+    name: 'GPT-5-Instant',
+    maxLength: 15100,
+    tokenLimit: 128000,
+    tokenLimitMargin: 3000,
+    maxMessages: 20,
+  },
+  [OpenAIModelID.GPT_5]: {
+    id: OpenAIModelID.GPT_5,
+    name: 'GPT-5',
+    maxLength: 15100,
+    tokenLimit: 128000,
+    tokenLimitMargin: 3000,
+    maxMessages: 20,
+  },
 };
+
